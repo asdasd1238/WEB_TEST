5a0cb7c8ef22fb10615d599a18984b545c2c19c0
asdasd1238 <zxcvbnm120567@gmail.com>
2017-05-20 18:26:28 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,81 @@
+// Google Analytics
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+// TODO add your tracking id here
+ga('create', 'tracking-id', 'auto');
+ga('send', 'pageview');
+
+function  langswitch_C()
+{
+
+  document.getElementById("f1").innerHTML="特色介紹";
+  document.getElementById("f2").innerHTML="APP介紹";
+  document.getElementById("f3").innerHTML="聯絡我們";
+  document.getElementById("f4").innerHTML="產 品 完 成 時 取 得 通 知";
+  document.getElementById("exampleModalLabel").innerHTML ="聯絡方式";
+  document.getElementById("6").innerHTML="信箱";
+  document.getElementById("7").innerHTML="想對我們說的話";
+  document.getElementById("8").innerHTML="取消";
+  document.getElementById("9").innerHTML="送出";
+  document.getElementById("feature").innerHTML="特色介紹";
+  document.getElementById("11").innerHTML="結合日曆";
+  document.getElementById("12").innerHTML="結合小遊戲";
+  document.getElementById("13").innerHTML="與寵物互動";
+  document.getElementById("14").innerHTML="幫您記錄未來的代辦事項和安排時間，及提醒您該日尚未完成事項";
+  document.getElementById("15").innerHTML="如果在您事先所訂定的時間完成您的代辦事項，可以獲得額外的獎勵";
+  document.getElementById("16").innerHTML="當完成您的代辦事項，會得到些許遊戲金幣及飼料，並可和您的寵物互動"
+  document.getElementById("fultxt").innerHTML="APP介紹";
+  document.getElementById("18").innerHTML="記下之後會做的事情<br/>並選擇是否進入互動模式";
+  document.getElementById("19").innerHTML="您的寵物會提醒你任何代辦事項<br/>並且可以看到目前寵物的情況";
+  document.getElementById("20").innerHTML="<br/>To-do Smart<br/>會用一些獎勵來鼓勵您完成項目";
+  document.getElementById("21").innerHTML="和寵物快樂了互動!<br/>在也不會對做事情感到無聊!!";
+  document.getElementById("22").innerHTML="注意!!&nbsp;&nbsp這個APP是免費的!"
+  document.getElementById("23").innerHTML="請花30秒替我們做個問卷來幫助我們改善APP<br/>您的每一個回饋對我們都很重要"
+  document.getElementById("24").innerHTML="&nbsp;&nbsp;OK,我願意提供我的意見";
+  document.getElementById("25").innerHTML="聯絡我們";
+  document.getElementById("26").innerHTML="如果您有任何問題 請email我們<br/>我們將會逐一回覆";
+  document.getElementById("a").style.display="inline";
+  document.getElementById("b").style.display="none";
+}
+function langswitch_E()
+{
+
+  document.getElementById("f1").innerHTML="Features";
+  document.getElementById("f2").innerHTML="Full Text";
+  document.getElementById("f3").innerHTML="Contact us";
+  document.getElementById("f4").innerHTML="Get Notified When Available";
+  document.getElementById("exampleModalLabel").innerHTML ="New message";
+  document.getElementById("6").innerHTML="Email";
+  document.getElementById("7").innerHTML="Message";
+  document.getElementById("8").innerHTML="Close";
+  document.getElementById("9").innerHTML="Send message";
+  document.getElementById("feature").innerHTML="Features";
+  document.getElementById("11").innerHTML="Calendar";
+  document.getElementById("12").innerHTML="Game";
+  document.getElementById("13").innerHTML="Pet";
+  document.getElementById("14").innerHTML="Help you to arrange time and manage your daily life.";
+  document.getElementById("15").innerHTML="If you reach somewhere on appointed time which is set beforehand, you will get additional awards.";
+  document.getElementById("16").innerHTML="When the schedule completed, you can earn golds to keep a pet, and interact with them."
+  document.getElementById("fultxt").innerHTML="Full Text";
+  document.getElementById("18").innerHTML="Write down the to-do list on  calendar.<br/>You can enter game mode.";
+  document.getElementById("19").innerHTML="It will remind you to do anything.<br/>You can see the state of your pet.";
+  document.getElementById("20").innerHTML="<br/>To-do Smart<br/>will motivate you to finsih items for getting  different rewards.";
+  document.getElementById("21").innerHTML="Interact happily with your pet.<br/>No more forget and feel Boring !!";
+  document.getElementById("22").innerHTML="It's Free!"
+  document.getElementById("23").innerHTML="Please help us improve by taking a 30sec survey.<br/>Your feedback is highly appreciated."
+  document.getElementById("24").innerHTML="&nbsp;&nbsp;Ok, I'd Like to Help";
+  document.getElementById("25").innerHTML="Contact us";
+  document.getElementById("26").innerHTML="If you have any question, please email us.";
+  document.getElementById("a").style.display="none";
+  document.getElementById("b").style.display="inline";
+}
